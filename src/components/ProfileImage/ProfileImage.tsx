868e2324af3c3e522d1d555713049a2d926b1b8f
Cameron Gray <this@cameronjgray.com>
2023-07-16 13:36:54 +0100

    added alt tag

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -1,7 +1,7 @@
 export function ProfileImage() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <img src="/headshot.webp" className="md:w-[30rem] w-60 rounded-full"></img>
+      <img src="/headshot.webp" className="md:w-[30rem] w-60 rounded-full" alt="Image of Cameron"></img>
 
       <div className="grid grid-cols-3 gap-2 mt-3">
         <a href="https://www.linkedin.com/in/cameronjgray47/" target="_blank"><img src="/linkedin.webp" alt="LinkedIn logo" className="2xl:w-10 2xl:h-10 w-8 h-8 cursor-pointer"></img></a>
